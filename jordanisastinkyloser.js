c1c9af44f6f640cb55f793fdf8f869340b59a5df
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2024-10-24 13:59:26 -0400

    Update jordanisastinkyloser.js

--- jordanisastinkyloser.js
+++ jordanisastinkyloser.js
@@ -1,8 +1,23 @@
-function hextobinary(hex) {
-  hex.split("");
-  for (i = 0; i < hex.length(); i++) {}
+function dectobinary(dec) {
+  let binary = "";
+  for (i = 7; i > -1; i--) {
+    if (dec >= 2 ** i) {
+      dec = dec - 2 ** i;
+      binary += "1";
+    } else {
+      binary += "0";
+    }
+  }
+  return binary;
 }
-function binarytohex(binary) {
-  binary.split("");
-  for (i = 0; i < binary.length(); i++) {}
+function binarytodec(binary) {
+  let decimal = 0;
+  for (i = binary.split("").length - 1; i > -1; i--) {
+    if (binary[i] == 1) {
+      decimal = decimal + 2 ** i;
+    }
+  }
+  return decimal;
 }
+console.log(binarytodec("11111111"));
+console.log(dectobinary("70"));
